3b19d5220d379cb32465b3e8651bf71c7f259f97
Jose Roberto Vidal <rvidal@civisanalytics.com>
2016-03-30 18:18:09 -0500

    Allow consumers to mock out Haml helpers

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -8,14 +8,33 @@ $stderr.sync = true
 # Signals the parent process that loading 'haml' worked
 STDOUT.print '**'
 
-ERROR_PREFIX = "error:\n"
+ERROR_PREFIX = "error:\n".freeze
 # Reads a random separator
 SEPARATOR = STDIN.gets
 
-def compile(filePath)
-  template = File.read filePath
+class Context
+  def noop(*_args)
+    '<noop></noop>'
+  end
+end
+
+def setup_context
+  methods_header = STDIN.gets
+
+  raise 'karma-ng-hamlt2js internal error: expecting METHODS' if methods_header != "METHODS:\n"
+
+  loop do
+    method_name = STDIN.gets
+    break if method_name == SEPARATOR
+    method_name = method_name[0...-1]
+    Context.send(:define_method, method_name, Context.instance_method(:noop))
+  end
+end
+
+def compile(file_path)
+  template = File.read file_path
   haml_engine = Haml::Engine.new template
-  output = haml_engine.render
+  output = haml_engine.render(Context.new)
 
   STDOUT.print output
   STDOUT.print SEPARATOR
@@ -26,6 +45,8 @@ rescue StandardError => e
 end
 
 begin
+  setup_context
+
   loop do
     line = STDIN.gets
     compile line[0...-1]
